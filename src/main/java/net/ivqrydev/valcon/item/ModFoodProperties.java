54f74268292689fc94488a8547df133fa0ffcd2d
IvqryDev <156384979+IvqryDev@users.noreply.github.com>
2026-02-18 10:29:53 -0600

    Added nourishment back to lembas.

--- src/main/java/net/ivqrydev/valcon/item/ModFoodProperties.java
+++ src/main/java/net/ivqrydev/valcon/item/ModFoodProperties.java
@@ -11,8 +11,8 @@ public class ModFoodProperties {
 
     static {
         FoodProperties.Builder builder = new FoodProperties.Builder()
-                .nutrition(16)
-                .saturationModifier(0.8f)
+                .nutrition(18)
+                .saturationModifier(1.0f)
                 .alwaysEdible();
 
         ResourceLocation comfortId = ResourceLocation.fromNamespaceAndPath("farmersdelight", "comfort");
@@ -23,6 +23,16 @@ public class ModFoodProperties {
                     .flatMap(BuiltInRegistries.MOB_EFFECT::getHolder)
                     .ifPresent(holder -> builder.effect(
                             () -> new MobEffectInstance(holder, 6000, 0, false, false, true), 1.0f)); // 5 min
+
+            
+        ResourceLocation nourishmentId = ResourceLocation.fromNamespaceAndPath("farmersdelight", "nourishment");
+        MobEffect nourishment = BuiltInRegistries.MOB_EFFECT.get(nourishmentId);
+
+        if (comfort != null) {
+            BuiltInRegistries.MOB_EFFECT.getResourceKey(nourishment)
+                    .flatMap(BuiltInRegistries.MOB_EFFECT::getHolder)
+                    .ifPresent(holder -> builder.effect(
+                            () -> new MobEffectInstance(holder, 2400, 0, false, false, true), 1.0f)); // 2 min
         }
 
         LEMBAS = builder.build();
