59778ddf4018db854283d3980c24f044a5294dfa
IvqryDev <noah7365@outlook.com>
2026-06-16 22:29:38 -0500

    Remove unneeded stuff in main mod file.

--- src/main/java/net/ivqrydev/valcon/Valcon.java
+++ src/main/java/net/ivqrydev/valcon/Valcon.java
@@ -1,9 +1,7 @@
 package net.ivqrydev.valcon;
 
-import com.mojang.logging.LogUtils;
 import net.ivqrydev.valcon.block.ModBlocks;
 import net.ivqrydev.valcon.block.entity.ModBlockEntities;
-import net.ivqrydev.valcon.block.entity.renderer.SoulForgeBlockEntityRenderer;
 import net.ivqrydev.valcon.effect.ModEffects;
 import net.ivqrydev.valcon.item.ModCreativeModeTabs;
 import net.ivqrydev.valcon.item.ModItems;
@@ -16,19 +14,14 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import org.slf4j.Logger;
 import net.minecraft.world.level.Level;
 
 @Mod(Valcon.MOD_ID)
 public class Valcon {
     public static final String MOD_ID = "valcon";
-
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public Valcon(IEventBus modEventBus) {
 
@@ -60,13 +53,6 @@ public class Valcon {
                 serverPlayer.setRespawnPosition(level.dimension(), pos, player.getYRot(), false, true);
                 event.setCanceled(true);
             }
-        }
-    }
-
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
-            event.registerBlockEntityRenderer(ModBlockEntities.SOUL_FORGE_BE.get(), SoulForgeBlockEntityRenderer::new);
         }
     }
 }
