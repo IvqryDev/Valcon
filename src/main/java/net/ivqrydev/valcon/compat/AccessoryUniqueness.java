a93976981638f1d730dd6a774449448d2684b6cb
IvqryDev <noah7365@outlook.com>
2026-09-12 01:30:20 -0500

    Fix tooltip deleting with AccessoryUniqueness.

--- src/main/java/net/ivqrydev/valcon/compat/AccessoryUniqueness.java
+++ src/main/java/net/ivqrydev/valcon/compat/AccessoryUniqueness.java
@@ -14,14 +14,19 @@ public final class AccessoryUniqueness {
         CanEquipCallback.EVENT.register(AccessoryUniqueness::onCanEquip);
     }
 
-    //Blocks player from equipping a talisman of the same type.
+    //Blocks player from equipping a talisman of the same type in more than one slot.
     private static TriState onCanEquip(ItemStack stack, SlotReference reference) {
         if (stack.isEmpty()) return TriState.DEFAULT;
 
         var capability = AccessoriesCapability.get(reference.entity());
         if (capability == null) return TriState.DEFAULT;
 
-        if (capability.isAnotherEquipped(stack, reference, stack.getItem())) {
+        var item = stack.getItem();
+
+        for (var equipped : capability.getAllEquipped()) {
+            if (equipped.reference().equals(reference)) continue;
+            if (equipped.stack().getItem() != item) continue;
+
             return TriState.FALSE;
         }
 
